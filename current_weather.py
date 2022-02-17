43d7ae2ec435e52ae69b393faaddceb1107813d1
Dan Smestad <hockeydan25@gmail.com>
2022-02-16 18:48:32 -0600

    update key useage to weather_key local.

--- current_weather.py
+++ current_weather.py
@@ -5,18 +5,18 @@ import requests
 from pprint import pprint
 import os
 
-# key =os.environ.get('WEATHER_KEY')
-# print(key)
+key = os.environ.get('WEATHER_KEY')
+#print(key)
 
-url = 'https://api.openweathermap.org/data/2.5/weather'#?q=minneapolis,mn,us&units=imperial&appid=1b190e134eae4fa3b246a84407c6307c'
+url = 'https://api.openweathermap.org/data/2.5/weather?q=minneapolis,mn,us&units=imperial&appid={key}'
 #We can adjust city, state, temp units. understand the url to make requests read url's documnet about the api.
 
 # users can request a change of city and location
-city= input('Enter city you would like: ')
-country= input('Enter country 2-letter code: ')
-location = f'{city}, {country}'
+# city= input('Enter city you would like: ')
+# country= input('Enter country 2-letter code: ')
+# location = f'{city}, {country}'
 
-query = {'q': 'minneapolis', 'units': 'metric', 'appid': '1b190e134eae4fa3b246a84407c6307c'}
+query = {'q': 'minneapolis', 'units': 'metric', 'appid': '{key}'}
 
 
 data= requests.get(url, params=query).json()  # fetch url api weblink.
