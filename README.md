dee468b07808f249c5465d7b15b3d3dc653f1195
Dan Smestad <hockeydan25@gmail.com>
2022-02-20 15:59:34 -0600

    comments updated, readme text updated. testing is good. no log file, need to review.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 Dan Smestad Capstone 2905 class Python API connections
 
 A few programs here demostrating the use of API's and using the data in a useful manner or output
-for users. 
+for users. use of api to get a photo jpeg file and saving with download_dog_cat.py.
+cat_fact, bitcoin, current_weather, and forecast simple api calls to get data.
+
+weather_forecast gets five day forecast for ciy country of choice. list of country codes, can be found,
+https://www.datahub.io/core/country-list.
+Program takes your city country and gets a forcast for five days from the time entered byt user. Temperture date and time a short description of skies and wind. Uses an API call for its data collection.
+Time choice is standard for readability.
--- weather_forecast.py
+++ weather_forecast.py
@@ -2,18 +2,18 @@
 Dan Smestad Capstone 2905 class Python API connections
 """
 
-import os                 # needed to get to global key where we set the weather_key key is saved could we encypt  
+import os  # needed to get to global key where we set the weather_key key is saved could we encypt  
 import requests
 from pprint import pprint  # makes printing out put cleaner. first time using this import.
 from datetime import datetime  # needed to deal with time fomort conversions. 
 
 key = os.environ.get('WEATHER_KEY')  # key will be local varial key forword and get the key value to insert into our url
-url = 'https://api.openweathermap.org/data/2.5/forecast' # updated key 
+url = 'https://api.openweathermap.org/data/2.5/forecast' # updated key openweather.org
 
-## TODO checks: spaces, spelling, more testing,comments, error handling complete
+# checks: spaces, spelling, more testing,comments, error handling complete
 def main():    
     location = get_location()
-    weather_data = get_forecast_weather(location,key)
+    weather_data = get_forecast_weather_location(location,key)
     weather_forecast = get_five_day_forecast(weather_data)    
     # print(f'Your current temp: {weather_forecast}f,local date and time is: {weather_forecast[1]}, skies are {weather_forecast[2]}, with a wind speed {weather_forecast[3]} in {location}.') 
            
@@ -30,7 +30,7 @@ def get_location():
     return location
 
 
-def get_forecast_weather(location,key):
+def get_forecast_weather_location(location,key):
     try:
         query = {'q': location, 'units': 'imperial', 'appid': key} # picking keys from dictionary 
         response = requests.get(url, params=query)  # request does the query from our params in query variable
@@ -41,20 +41,19 @@ def get_forecast_weather(location,key):
          print(e)
          print('There was an error making the request.')   
 
-    ## TODO international time conversion
-    ## TODO windspeed three hour intervals - may need a new url to choose or maybe 5 day has that option.
-    ## TODO add a 5 day time frame. Unix Time or UTC. 
-def get_five_day_forecast(data):
+    # notedinternational time conversion
+    # weather forcast windspeed three hour intervals - url to choose or maybe 5 day has that option.
+    # add a 5 day time frame. 
+def get_five_day_forecast(weather_data):
     try:
-        forecasted_weather = data['list']
+        forecasted_weather = weather_data['list']  # putting to list so data can be retieved
         for forecasts in forecasted_weather:
             temp = forecasts['main']['temp']   # getting data for temp
-            time = forecasts['dt']
-            time_date = datetime.fromtimestamp(time)  #  non internalional format  
-            description = forecasts['weather'][0]['description']
-            wind = forecasts['wind']['speed']            
-            #return temp, time_date, description, wind
-            print(f'Temperture data {temp}F\nlocal date and time {time_date}\nskies are {description}\nhaving a wind speed of {wind}.') 
+            time = forecasts['dt']             # gets timedate info, needs converting 
+            time_date = datetime.fromtimestamp(time)  # internalional format  datetime converts
+            description = forecasts['weather'][0]['description']  # gets short description of skies
+            wind = forecasts['wind']['speed']                     # wind speed data  
+            pprint(f'Temperture data {temp}F\nlocal date and time {time_date}\nskies are {description}\nhaving a wind speed of {wind}.') 
     except Exception as e:
         print(e)
         print('There was an error making the request.')  # general error message.  
